0b0805b337c364214bf17f126a60564f86668839
Pavel Golubev <pavelgolubew@gmail.com>
2022-08-17 23:39:09 +0300

    Homework's tasks 1, 2, 3 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Task 01
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+//        Task02
+        String fullNameUpper = lastName.toUpperCase() + " " + firstName.toUpperCase() + " " + middleName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpper);
+
+        task03();
+    }
+
+    public static void task03() {
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'е'));
     }
 }
